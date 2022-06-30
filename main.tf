463c0621dc83bb47d854d746575e1ed35fb9763b
r26robson <r26robson2hotmail.com>
2022-06-30 12:53:45 -0300

    criando maquinas em outra r4egião e separando arquivos

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region = "us-east-2"
 }
 
+provider "aws" {
+  alias = "us-east-1"
+  version = "~> 2.0"
+  region = "us-east-1"
+}
+
 resource "aws_instance" "terraform" {
   count = "3"
   ami = "ami-081c75eaeac28ac34"
@@ -34,6 +40,18 @@ resource "aws_instance" "terraform4" {
     Name = "terraform4"
   }
   vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+# Exemplo de criação de instancia EC2 em outra região
+resource "aws_instance" "terraform5" {
+  provider = aws.us-east-1
+  ami = "ami-0cff7528ff583bf9a"
+  instance_type = "t2.micro"
+  key_name = "aws-terraform-nv"
+  tags = {
+    Name = "terraform5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh-us-east-1.id}"]
 }
 
 # terraform show (mostra como ta sua estrutura/ambiente na AWS)
--- security-group.tf
+++ security-group.tf
@@ -1,16 +1,34 @@
 # Exemplo criação security group
 resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
+  name        = "acesso-ssh-ohio"
+  description = "acesso-ssh-ohio"
 
   ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = ["177.67.159.155/32"]
+    cidr_blocks      = ["177.67.159.140/32"]
   }
 
   tags = {
-    Name = "ssh"
+    Name = "acesso-ssh"
+  }
+}
+
+# Exemplo criação security group
+resource "aws_security_group" "acesso-ssh-us-east-1" {
+  provider = aws.us-east-1
+  name        = "acesso-ssh-nv"
+  description = "acesso-ssh-nv"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["177.67.159.140/32"]
+  }
+
+  tags = {
+    Name = "acesso-ssh"
   }
 }
